7d04e85b1a510df1b3d1f1213d25935245aa0ec4
gfxmonk <tim@gfxmonk.net>
2011-02-27 12:04:35 +1100

    version 0.5.1

--- conf.py
+++ conf.py
@@ -45,9 +45,9 @@ copyright = u'2010, Tim Cuthbertson'
 # built documents.
 #
 # The short X.Y version.
-version = '0.5'
+version = '0.5.1'
 # The full version, including alpha/beta/rc tags.
-release = '0.5'
+release = '0.5.1'
 
 autodoc_default_flags = ['members']
 
